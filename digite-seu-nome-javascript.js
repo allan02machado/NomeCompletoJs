b79d7f5f116155df7564d0840b67f53c42e9cb8c
allan02machado <70041162+allan02machado@users.noreply.github.com>
2021-04-25 16:09:08 -0300

    Atividade nome completo em JS

---
+++ digite-seu-nome-javascript.js
@@ -0,0 +1,6 @@
+let nome = prompt("Digite seu nome: ");
+let sobreNome = prompt("Digite seu sobrenome: ");
+let nomeCompleto = alert ((nome)+(sobreNome));
+
+alert("Confirme para gerar seu nome catálogo");
+alert(`Seu nome catálogo é: ${sobreNome.toLocaleUpperCase()}, ${nome}`);
